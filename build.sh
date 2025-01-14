ce66b736fe6ff08d94d51f2b1735f642f2421c6e
MarcosIgnacioo <magma_ruby145@hotmail.com>
2025-01-14 00:33:04 +0000

    wep

---
+++ build.sh
@@ -0,0 +1,14 @@
+if [[ $1 == "b" ]]; then
+   echo "BUILDING PROJECT"
+   go run main.go
+else
+   echo "NOT BUILDING PROJECT JUST COPYING MOST RECENT COMPILED VERSION"
+fi
+echo "removiendo contenido de /srv/http"
+sudo rm -r /srv/http/*
+echo "copiando doce40BUILD a srv/http"
+sudo cp -r ./doce40BUILD/*  /srv/http/
+echo "removiendo htaccess de srv/http"
+sudo rm /srv/http/.htaccess
+echo "copiando htacces a srv/http"
+sudo cp -r ./.htaccess  /srv/http/
